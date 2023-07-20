dbd112d88cfcd55b4185d60cedf6fa0b5bd5d5b3
Taran2626 <taran02611@gmail.com>
2023-07-21 01:25:51 +0530

    Initial Commit

---
+++ KlarnaTest/KlarnaTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  KlarnaTestApp.swift
+//  KlarnaTest
+//
+//  Created by Taranjeet Kaur on 21/07/23.
+//
+
+import SwiftUI
+
+@main
+struct KlarnaTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
